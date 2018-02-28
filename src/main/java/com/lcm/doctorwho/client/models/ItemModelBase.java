dd4be7ce45585c1c2e98ffca5225aa00e07c1a65
HoldYourWaffle <ravivanrooijen@live.nl>
2018-02-28 17:32:59 +0000

    Annotate members that are needed by Regeneration
    Will get more processing in the future

--- src/main/java/com/lcm/doctorwho/client/models/ItemModelBase.java
+++ src/main/java/com/lcm/doctorwho/client/models/ItemModelBase.java
@@ -1,7 +1,10 @@
 package com.lcm.doctorwho.client.models;
 
+import com.lcm.regeneration.util.RegenRequires;
+
 import net.minecraftforge.common.model.IModelState;
 
+@RegenRequires
 public interface ItemModelBase {
 	
 	void renderModel(float scale);
--- src/main/java/com/lcm/doctorwho/utils/ATGSoundEvent.java
+++ src/main/java/com/lcm/doctorwho/utils/ATGSoundEvent.java
@@ -1,11 +1,13 @@
 package com.lcm.doctorwho.utils;
 
 import com.lcm.doctorwho.AcrossTheGalaxy;
+import com.lcm.regeneration.util.RegenRequires;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 
-public class ATGSoundEvent extends SoundEvent { // TODO in both
+@RegenRequires
+public class ATGSoundEvent extends SoundEvent {
 	
 	public ATGSoundEvent(String name) {
 		super(new ResourceLocation(AcrossTheGalaxy.MODID, name));
