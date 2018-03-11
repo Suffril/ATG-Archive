80ba6035bd6f9fe539b680970558abad982e9749
HoldYourWaffle <ravivanrooijen@live.nl>
2018-03-11 23:20:02 +0100

    Cleanup scripts
    Why the fuck was all of this working first time. That's not how it's supposed to go.

--- cleanme.sh
+++ cleanme.sh
@@ -1,2 +1,12 @@
-astyle --style=java --indent=tab --indent-switches --recursive src/main/java/*.java
-./astyle-clean.sh
+if [ ! -d "formatter" ]; then
+	echo "Formatter not found, downloading..."
+	wget http://download2194.mediafire.com/bim6zi6ra0ng/gi52dtm66ib1952/atg-formatter.tar.gz -nv || { echo 'Download failed :(' ; exit 1; }
+	echo "Download successful :)"
+	echo "Extracting..."
+	tar xf atg-formatter.tar.gz || { echo 'Extraction failed :(' ; exit 1; }
+	echo "Extraction complete, cleaning up"
+	rm atg-formatter.tar.gz || { echo 'Something broke really badly' ; exit 1; }
+fi
+
+java -jar formatter/plugins/org.eclipse.osgi_3.11.2.v20161107-1947.jar -application org.eclipse.jdt.core.JavaCodeFormatter -config formatter/format-settings.ini src/*
+rm -r workspace
